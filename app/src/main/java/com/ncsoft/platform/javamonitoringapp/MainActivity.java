c8bdddfed11a2a998c7bc113aa9241c4d7757972
skwangh <skwangh@gmail.com>
2015-11-15 16:26:48 +0900

    좌측 메뉴 뼈대 적용

--- app/src/main/java/com/ncsoft/platform/javamonitoringapp/MainActivity.java
+++ app/src/main/java/com/ncsoft/platform/javamonitoringapp/MainActivity.java
@@ -1,10 +1,9 @@
 package com.ncsoft.platform.javamonitoringapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,8 +12,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ncsoft.platform.javamonitoringapp.fragments.PerformanceMonitorFragment;
+import com.ncsoft.platform.javamonitoringapp.fragments.StatusCheckFragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private StatusCheckFragment statusCheckFragment;
+    private PerformanceMonitorFragment performanceMonitorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +27,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +36,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        statusCheckFragment = new StatusCheckFragment();
+
+        performanceMonitorFragment = new PerformanceMonitorFragment();
     }
 
     @Override
@@ -78,12 +78,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
         int id = item.getItemId();
 
         if (id == R.id.nav_camara) {
+            fragmentManager.beginTransaction().replace(R.id.fragment_main, statusCheckFragment).addToBackStack(null).commit();
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            fragmentManager.beginTransaction().replace(R.id.fragment_main, performanceMonitorFragment).addToBackStack(null).commit();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
