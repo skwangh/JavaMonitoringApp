52e46ff4d36e88c990b8dded42e7855910aff316
skwangh <skwangh@gmail.com>
2015-12-11 09:07:54 +0900

    ColorUtil 추가

--- app/src/main/java/com/ncsoft/platform/javamonitoringapp/fragments/PerformanceMonitorFragment.java
+++ app/src/main/java/com/ncsoft/platform/javamonitoringapp/fragments/PerformanceMonitorFragment.java
@@ -18,7 +18,9 @@ import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.ncsoft.platform.javamonitoringapp.R;
+import com.ncsoft.platform.javamonitoringapp.adapters.LegendAdapter;
 import com.ncsoft.platform.javamonitoringapp.utils.MpChartUtils;
 
 import java.util.ArrayList;
@@ -59,63 +61,6 @@ public class PerformanceMonitorFragment extends Fragment {
         return view;
     }
 
-//    private void customLegend() {
-//
-//        int colorcodes[] = legend.getColors();
-//        String lables[] = legend.getLabels();
-//
-//        for (int i = 0; i <  colorcodes.length ; i++) {
-//            LinearLayout.LayoutParams parms_left_layout = new LinearLayout.LayoutParams(
-//                    LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-//            parms_left_layout.weight = 1F;
-//            LinearLayout left_layout = new LinearLayout(getActivity());
-//            left_layout.setOrientation(LinearLayout.HORIZONTAL);
-//            left_layout.setGravity(Gravity.CENTER);
-//            left_layout.setLayoutParams(parms_left_layout);
-//
-//            LinearLayout.LayoutParams parms_legen_layout = new LinearLayout.LayoutParams(
-//                    20, 20);
-//            parms_legen_layout.setMargins(0, 0, 20, 0);
-//            LinearLayout legend_layout = new LinearLayout(getActivity());
-//            legend_layout.setLayoutParams(parms_legen_layout);
-//            legend_layout.setOrientation(LinearLayout.HORIZONTAL);
-//            legend_layout.setBackgroundColor(colorcodes[i]);
-//            left_layout.addView(legend_layout);
-//
-//            TextView txt_unit = new TextView(getActivity());
-//            txt_unit.setText(legend.getLabel(i));
-//            left_layout.addView(txt_unit);
-//
-//            LinearLayout.LayoutParams parms_middle_layout = new LinearLayout.LayoutParams(
-//                    LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-//            parms_middle_layout.weight = 1F;
-//            LinearLayout middle_layout = new LinearLayout(getActivity());
-//            middle_layout.setOrientation(LinearLayout.HORIZONTAL);
-//            middle_layout.setGravity(Gravity.CENTER);
-//            middle_layout.setLayoutParams(parms_middle_layout);
-//
-//            TextView txt_leads = new TextView(getActivity());
-//            txt_leads.setText("450");
-//            middle_layout.addView(txt_leads);
-//
-//            LinearLayout.LayoutParams parms_right_layout = new LinearLayout.LayoutParams(
-//                    LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-//            parms_right_layout.weight = 1F;
-//            LinearLayout right_layout = new LinearLayout(getActivity());
-//            right_layout.setOrientation(LinearLayout.HORIZONTAL);
-//            right_layout.setGravity(Gravity.CENTER);
-//            right_layout.setLayoutParams(parms_right_layout);
-//
-//            TextView txt_leads_percentage = new TextView(getActivity());
-//            txt_leads_percentage.setText(lables[i]);
-//            right_layout.addView(txt_leads_percentage);
-//
-//            layout.addView(left_layout);
-//            layout.addView(middle_layout);
-//            layout.addView(right_layout);
-//
-//        }
-//    }
 
     private void addItemsOnChartTypeSpinner() {
 
@@ -141,20 +86,14 @@ public class PerformanceMonitorFragment extends Fragment {
     }
 
     private void addItemsOnListChartItem() {
-        Legend legend = chart.getLegend();
-        int colors[] = legend.getColors();
-        String lables[] = legend.getLabels();
-
         chartItemListView = (ListView) view.findViewById(R.id.list_view_chart_item);
-        List<String> list = new ArrayList<String>();
-        for (int i = 0 ; i < lables.length ; i++) {
-            list.add(lables[i]);
-        }
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), R.layout.layout_legend, list);
-        chartItemListView.setAdapter(dataAdapter);
-//        chartItemListView.setSelection(9);
-//        chartItemListView.getChildAt(9).setBackgroundColor(Color.BLUE);
+        List<LineDataSet> lineDataSetList = chart.getData().getDataSets();
+
+        LegendAdapter legendAdapter = new LegendAdapter(lineDataSetList);
+
+        chartItemListView.setAdapter(legendAdapter);
+
     }
 
 }
---
+++ app/src/main/java/com/ncsoft/platform/javamonitoringapp/utils/ColorUtils.java
@@ -0,0 +1,71 @@
+package com.ncsoft.platform.javamonitoringapp.utils;
+
+import android.graphics.Color;
+
+/**
+ * Created by skwangh on 2015-12-11.
+ */
+public class ColorUtils {
+
+    public int getColor(int index) {
+        return colorSet[index % colorSet.length];
+    }
+
+    private static int[] colorSet = new int[] {
+            Color.rgb(255,0,0)
+            ,Color.rgb(255,153,0)
+            ,Color.rgb(204,255,0)
+            ,Color.rgb(51,255,0)
+            ,Color.rgb(0,255,255)
+            ,Color.rgb(0,102,255)
+            ,Color.rgb(51,0,255)
+            ,Color.rgb(204,0,255)
+            ,Color.rgb(255,0,153)
+            ,Color.rgb(255,128,128)
+            ,Color.rgb(255,204,128)
+            ,Color.rgb(229,255,128)
+            ,Color.rgb(153,255,128)
+            ,Color.rgb(128,255,178)
+            ,Color.rgb(128,179,255)
+            ,Color.rgb(153,128,255)
+            ,Color.rgb(230,128,255)
+            ,Color.rgb(255,128,204)
+            ,Color.rgb(229,0,0)
+            ,Color.rgb(0,204,82)
+            ,Color.rgb(0,204,204)
+            ,Color.rgb(178,107,0)
+            ,Color.rgb(143,179,0)
+            ,Color.rgb(0,71,179)
+            ,Color.rgb(36,0,179)
+            ,Color.rgb(143,0,179)
+            ,Color.rgb(178,0,107)
+            ,Color.rgb(178,89,89)
+            ,Color.rgb(178,143,89)
+            ,Color.rgb(161,179,89)
+            ,Color.rgb(107,179,89)
+            ,Color.rgb(89,125,179)
+            ,Color.rgb(107,89,179)
+            ,Color.rgb(161,89,179)
+            ,Color.rgb(179,89,143)
+            ,Color.rgb(153,0,0)
+            ,Color.rgb(0,153,153)
+            ,Color.rgb(77,153,107)
+            ,Color.rgb(102,128,0)
+            ,Color.rgb(25,128,0)
+            ,Color.rgb(0,128,128)
+            ,Color.rgb(26,0,128)
+            ,Color.rgb(102,0,0)
+            ,Color.rgb(102,61,0)
+            ,Color.rgb(0,102,41)
+            ,Color.rgb(0,102,102)
+            ,Color.rgb(0,41,102)
+            ,Color.rgb(82,0,102)
+            ,Color.rgb(102,0,61)
+            ,Color.rgb(102,51,51)
+            ,Color.rgb(102,82,51)
+            ,Color.rgb(92,102,51)
+            ,Color.rgb(51,71,102)
+            ,Color.rgb(61,51,102)
+            ,Color.rgb(102,51,82)
+    };
+}
